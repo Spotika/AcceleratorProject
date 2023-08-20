71f37de8de25275a2257eece82c55323d6d6866b
spotika4 <andrey.a.cherkashin@gmail.com>
2023-08-20 20:54:10 +0300

    Добавлен каркас программы без приложений

--- Games/GameManager.py
+++ Games/GameManager.py
@@ -4,7 +4,16 @@ class GameManager:
 
     @classmethod
     def init(cls) -> None:
-        """:
+        """
         Инициализация
         """
 
+    @classmethod
+    def start_game(cls, name: str) -> None:
+        """
+        Начинает игру с именем name
+        :param name: имя игры
+        :return:
+        """
+
+    
